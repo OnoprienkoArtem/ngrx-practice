57730b0957cca035488cfc20347d9cd237acd1f0
OnoprienkoArtem <o.artem.e@gmail.com>
2020-03-15 22:08:16 +0200

    update project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ngrx-practice');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngrx-practice!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ngrx-practice app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './counter.reducer';
-import { CounterComponent } from './counter/counter.component';
-import { Counter2Component } from './counter2/counter2.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent,
-    Counter2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot({ count: counterReducer })
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
