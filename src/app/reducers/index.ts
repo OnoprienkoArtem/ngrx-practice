1748841f00ff51eeb5a139d4fac8d94e15696834
OnoprienkoArtem <o.artem.e@gmail.com>
2020-03-17 00:01:19 +0200

    add reducers folder

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -7,3 +7,14 @@ import {
 } from '@ngrx/store';
 
 import { environment } from '../../environments/environment';
+
+export interface State {
+
+}
+
+export const reducers: ActionReducerMap<State> = {
+
+};
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+
