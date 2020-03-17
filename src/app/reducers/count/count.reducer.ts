d859a5dede9a7bb0822efd29c646e545a9ff4a94
OnoprienkoArtem <o.artem.e@gmail.com>
2020-03-17 23:38:12 +0200

    add count reducer

---
+++ src/app/reducers/count/count.reducer.ts
@@ -0,0 +1,13 @@
+export interface CountState {
+  count: number;
+  updatedAt: number;
+}
+
+const initialState = {
+  count: 0,
+  updatedAt: Date.now()
+};
+
+export const countReducer = (state = initialState, action) => {
+  return state;
+};
