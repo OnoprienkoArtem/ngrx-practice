af5e37d36cac7d2b80522ad2faedbb9dd213b311
OnoprienkoArtem <o.artem.e@gmail.com>
2020-03-18 22:01:52 +0200

    add count reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -5,15 +5,15 @@ import {
   createSelector,
   MetaReducer
 } from '@ngrx/store';
-
+import { CountState, countReducer } from './count/count.reducer';
 import { environment } from '../../environments/environment';
 
 export interface State {
-
+  count: CountState;
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  count: countReducer
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
