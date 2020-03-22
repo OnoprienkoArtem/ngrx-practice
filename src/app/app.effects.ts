57ea07e09015c4631fcf6f02f6a042d5357a641b
OnoprienkoArtem <o.artem.e@gmail.com>
2020-03-22 13:48:42 +0200

    add effects for update date

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import {Actions, Effect, ofType} from '@ngrx/effects';
+import {countActionsType, CountUpdatedAtAction} from './reducers/count/count.action';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class AppEffects {
   constructor(private actions$: Actions) {}
+
+  @Effect()
+  updatedAt$() {
+    return this.actions$.pipe(
+      ofType(countActionsType.increase, countActionsType.decrease, countActionsType.clear),
+      map(() => {
+        return new CountUpdatedAtAction({
+          updatedAt: Date.now()
+        });
+      })
+    );
+  }
 }
 
 
